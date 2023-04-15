38b3192c78a5571ff57a49feae54ef14188fbc1b
Charanopa <129950550+Charanopa@users.noreply.github.com>
2023-04-15 18:12:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['13129086'])
-API_HASH = environ['e15ddcaf8966750234b4b2f042f151d6']
-BOT_TOKEN = environ['6251639460:AAH0H1LKTUycSY1COE355demsWbc4IAcnd0']
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
